be3745fe9931646edf456752f125fadab2a6443d
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-08-14 13:39:24 +0300

    Q-1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+            #region Q-1
+            public class Range<T> where T : IComparable<T>
         {
-            Console.WriteLine("Hello, World!");
+            public T Min { get; }
+            public T Max { get; }
+
+            public Range(T min, T max)
+            {
+                if (min.CompareTo(max) > 0)
+                    throw new ArgumentException("Minimum value cannot be greater than maximum value.");
+
+                Min = min;
+                Max = max;
+            }
+
+            public bool IsInRange(T value)
+            {
+                return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+            }
+
+            public dynamic Length()
+            {
+                return (dynamic)Max - (dynamic)Min;
+            }
+            #endregion
+
+
         }
     }
 }
