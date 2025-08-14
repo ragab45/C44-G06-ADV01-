db722176a51ddb056d5a708c8858a45b0f9d6e5a
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-08-14 13:40:02 +0300

    Q-2

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,31 +1,52 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
-        
-            #region Q-1
-            public class Range<T> where T : IComparable<T>
+
+        #region Q-1
+        //    public class Range<T> where T : IComparable<T>
+        //{
+        //    public T Min { get; }
+        //    public T Max { get; }
+
+        //    public Range(T min, T max)
+        //    {
+        //        if (min.CompareTo(max) > 0)
+        //            throw new ArgumentException("Minimum value cannot be greater than maximum value.");
+
+        //        Min = min;
+        //        Max = max;
+        //    }
+
+        //    public bool IsInRange(T value)
+        //    {
+        //        return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        //    }
+
+        //    public dynamic Length()
+        //    {
+        //        return (dynamic)Max - (dynamic)Min;
+        //    }
+        #endregion
+        #region Q-2
+        public class ArrayListReverser
         {
-            public T Min { get; }
-            public T Max { get; }
-
-            public Range(T min, T max)
+            public static void Reverse(ArrayList list)
             {
-                if (min.CompareTo(max) > 0)
-                    throw new ArgumentException("Minimum value cannot be greater than maximum value.");
+                int left = 0;
+                int right = list.Count - 1;
 
-                Min = min;
-                Max = max;
-            }
+                while (left < right)
+                {
+                    var temp = list[left];
+                    list[left] = list[right];
+                    list[right] = temp;
 
-            public bool IsInRange(T value)
-            {
-                return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
-            }
-
-            public dynamic Length()
-            {
-                return (dynamic)Max - (dynamic)Min;
+                    left++;
+                    right--;
+                }
             }
             #endregion
 
