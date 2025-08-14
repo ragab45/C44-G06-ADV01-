ec82061aa77fdfcdac59f284808ce7037a1fc267
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-08-14 14:00:15 +0300

    V-2

--- Assemente/Program.cs
+++ Assemente/Program.cs
@@ -13,27 +13,45 @@
         
 namespace SessionDemoAdvanced
     {
-        public static class Helper
+        //    public static class Helper
+        //    {
+        //        // 1 reference
+        //        public static void Swap<T>(ref T x, ref T y)
+        //        {
+        //            T temp = x;
+        //            x = y;
+        //            y = temp;
+        //        }
+
+        //        /*public static void Swap(ref int x, ref int y)
+        //        {
+        //            int temp = x;
+        //            x = y;
+        //            y = temp;
+        //        }*/
+        //    }
+        //}
+        #endregion
+        #region V-2
+        public class EvenNumberFilter
         {
-            // 1 reference
-            public static void Swap<T>(ref T x, ref T y)
+            public static List<int> GetEvenNumbers(List<int> numbers)
             {
-                T temp = x;
-                x = y;
-                y = temp;
+                List<int> evens = new List<int>();
+
+                foreach (var num in numbers)
+                {
+                    if (num % 2 == 0)
+                    {
+                        evens.Add(num);
+                    }
+                }
+
+                return evens;
             }
+            #endregion
 
-            /*public static void Swap(ref int x, ref int y)
-            {
-                int temp = x;
-                x = y;
-                y = temp;
-            }*/
+
         }
-    }
-    #endregion
-
-
-}
     
 
