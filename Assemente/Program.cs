06341b64522f16b0c9adc0550e69e2adda19a72a
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-08-14 13:58:22 +0300

    V-1

--- Assemente/Program.cs
+++ Assemente/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Assemente
+﻿namespace Demo
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     internal class Program
     {
-        static void Main(string[] args)
+       
+            #region V-1
+        
+namespace SessionDemoAdvanced
+    {
+        public static class Helper
         {
-            Console.WriteLine("Hello, World!");
+            // 1 reference
+            public static void Swap<T>(ref T x, ref T y)
+            {
+                T temp = x;
+                x = y;
+                y = temp;
+            }
+
+            /*public static void Swap(ref int x, ref int y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }*/
         }
     }
+    #endregion
+
+
 }
+    
+
